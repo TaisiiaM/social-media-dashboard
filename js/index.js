fd9909f8364a73bc480ad42589c7da712a997d4f
TaisiiaM <mukolenkotaisiya@gmail.com>
2020-04-22 14:41:55 +0300

    code

--- js/index.js
+++ js/index.js
@@ -1,15 +1,10 @@
-const btnSwitch = document.querySelector('.switch-btn');
-btnSwitch.addEventListener('click', () => btnSwitch.classList.toggle('switch-on'));
+const btnSwitch = document.querySelector(".switch-btn");
+btnSwitch.addEventListener("click", () =>
+  btnSwitch.classList.toggle("switch-on")
+);
 
-let head = window.document.getElementsByTagName('head')[0];
-
-function includeCSS(aFile){
-  let style = window.document.createElement('link');
-  style.href = aFile;
-  style.rel = 'stylesheet';
-  head.appendChild(style);
-}
-includeCSS('css/index.css');
-
-btnSwitch.addEventListener('click', () => includeCSS('css/dark.css'))
-    //includeCSS('css/dark.css');
+const body = document.querySelector("body");
+btnSwitch.addEventListener("click", () => {
+  body.classList.toggle("light-theme");
+  body.classList.toggle("dark-theme");
+});
